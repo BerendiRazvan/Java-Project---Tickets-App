7a655d8b2cded8b9ef11fedb0ca4071dfc27c973
Răzvan Berendi <74509014+BerendiRazvan@users.noreply.github.com>
2022-03-08 15:03:10 +0200

    Add files via upload

---
+++ MPP_Proiect/MPP_Proiect/domain/Bilet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_Proiect.domain
+{
+    class Bilet
+    {
+        private string numeCumparator { get; set; }
+        private int locuri { get; set; }
+        private Spectacol spectacol { get; set; }
+
+        public Bilet(string numeCumparator, int locuri)
+        {
+            this.numeCumparator = numeCumparator;
+            this.locuri = locuri;
+        }
+
+        public override string ToString()
+        {
+            return "Bilet{" +
+                "numeCumparator='" + numeCumparator + '\'' +
+                ", locuri=" + locuri +
+                ", spectacol=" + spectacol +
+                '}';
+        }
+    }
+}
---
+++ MPP_Proiect/MPP_Proiect/domain/Spectacol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_Proiect.domain
+{
+    class Spectacol : Entity<long>
+    {
+        public string numeArtist { get; set; }
+        public DateTime data { get; set; }
+        public Locatie locatie { get; set; }
+        public int bileteDisponibile { get; set; }
+        public int bileteVandute { get; set; }
+
+        public Spectacol(string numeArtist, DateTime data, Locatie locatie, int bileteDisponibile, int bileteVandute)
+        {
+            this.numeArtist = numeArtist;
+            this.data = data;
+            this.locatie = locatie;
+            this.bileteDisponibile = bileteDisponibile;
+            this.bileteVandute = bileteVandute;
+        }
+
+
+        public override string ToString()
+        {
+            return "Spectacol{" +
+                "id=" + id +
+                ", numeArtist='" + numeArtist + '\'' +
+                ", data=" + data +
+                ", locatie=" + locatie +
+                ", bileteDisponibile=" + bileteDisponibile +
+                ", bileteVandute=" + bileteVandute +
+                '}';
+        }
+    }
+}
